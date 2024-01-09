6fe7e23dec4fb8d9d73d4848fdda8c363e8f2d71
BarnabedevWebmobile <barnabe.dwwm48@gmail.com>
2024-01-09 17:00:28 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- js/script.js
+++ js/script.js
@@ -1,4 +1,49 @@
-const intervalexit= setInterval(exit, 10000)
+const intervalexit = setInterval(exit, 3000)
+
+function showstep2(){
+    let step1 = document.getElementById('step1')
+    let step2 = document.getElementById('step2')
+    let title2 = document.getElementById('title2')
+    let poeme2 = document.getElementById('poeme2')
+    let autor2 = document.getElementById('autor2')
+    
+    step1.style.display = "none";
+    step2.style.display = "block";
+
+}
+
+function showstep3(){
+    let step3 = document.getElementById('step3')
+    let title3 = document.getElementById('title3')
+    let poeme3 = document.getElementById('poeme3')
+    let autor3 = document.getElementById('autor3')
+    
+    step1.style.display = none;
+    step2.style.display = block;
+
+}
+
+function showstep4(){
+    let step4 = document.getElementById('step4')
+    let title4 = document.getElementById('title4')
+    let poeme4 = document.getElementById('poeme4')
+    let autor4 = document.getElementById('autor4')
+    
+    step1.style.display = none;
+    step2.style.display = block;
+
+}
+
+function showstep5(){
+    let step5 = document.getElementById('step5')
+    let title5 = document.getElementById('title5')
+    let poeme5 = document.getElementById('poeme5')
+    let autor5 = document.getElementById('autor5')
+    
+    step1.style.display = none;
+    step2.style.display = block;
+
+}
 
 function start(){
     let step1 = document.getElementById('step1')
@@ -22,6 +67,9 @@ function start(){
 
         intervalexit
     },"2000");
+    setTimeout(()=>{
+        showstep2()
+    }, "11000")
 }
 
 
@@ -42,3 +90,4 @@ function exit(){
         autor1.style.opacity = 0;
     },"29000");
 }
+
